007dea54eaf1469c1a7a4b949685ed768d004f58
phamthangph13 <winnieph13@gmail.com>
2026-01-19 18:49:12 +0700

    pss

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,11 @@
+public class PlayerMovement : MonoBehaviour
+{
+// Update is called once per frame
+void Update()
+{
+var worldPoint =
+Camera.main.ScreenToWorldPoint(Input.mousePosition);
+worldPoint.z = 0;
+transform.position = worldPoint;
+}
+}
