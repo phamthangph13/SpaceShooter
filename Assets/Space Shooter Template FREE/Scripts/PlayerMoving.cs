54b9346d1cb00f7efe25e833867dd27d140affa2
phamthangph13 <winnieph13@gmail.com>
2026-02-01 22:09:31 +0700

    push demo part 3

--- Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
+++ Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
@@ -18,6 +18,10 @@ public class PlayerMoving : MonoBehaviour {
 
     [Tooltip("offset from viewport borders for player's movement")]
     public Borders borders;
+    
+    [Tooltip("Player movement speed")]
+    public float speed = 10f;
+
     Camera mainCamera;
     bool controlIsActive = true; 
 
@@ -41,12 +45,12 @@ public class PlayerMoving : MonoBehaviour {
         {
 #if UNITY_STANDALONE || UNITY_EDITOR    //if the current platform is not mobile, setting mouse handling 
 
-            if (Input.GetMouseButton(0)) //if mouse button was pressed       
-            {
-                Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition); //calculating mouse position in the worldspace
-                mousePosition.z = transform.position.z;
-                transform.position = Vector3.MoveTowards(transform.position, mousePosition, 30 * Time.deltaTime);
-            }
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(horizontal, vertical, 0f);
+            
+            transform.position += movement * speed * Time.deltaTime;
+
 #endif
 
 #if UNITY_IOS || UNITY_ANDROID //if current platform is mobile, 
